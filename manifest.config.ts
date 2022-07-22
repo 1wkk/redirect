869048af066aced389dc9d69324b80e9a1605db9
echoeureka <iherewithmyheart@gmail.com>
2022-07-22 22:47:41 +0800

    ✨ crx icon

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,6 +5,12 @@ export default defineManifest({
   name: 'Redirect',
   manifest_version: 3,
   version: '0.0.1',
+  icons: {
+    '16': 'icon.png',
+    '32': 'icon.png',
+    '48': 'icon.png',
+    '128': 'icon.png'
+  },
   content_scripts: [
     {
       js: ['src/content.ts'],
